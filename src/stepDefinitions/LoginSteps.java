7fe5f51a8dc03460ccec46d9b3e24b98b689bd62
Luis Gonzaga <luis.gonzaga@intelygenz.com>
2019-10-21 09:20:59 +0200

    setup

---
+++ src/stepDefinitions/LoginSteps.java
@@ -0,0 +1,25 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginSteps {
+	
+	@Given("the user is on the login page")
+	public void userOnLoginPage() {
+		
+	}
+	
+
+	@When("the user enters valid credentials")
+	public void userEntersValidCredentials() {
+		
+	}
+	
+	@Then("the user should be able to view account balance")
+	public void userViewsAccountBalance() {
+		
+	}
+	
+}
